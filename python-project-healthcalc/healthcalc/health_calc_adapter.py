8f7d7d86d1baa9ee6343a83266ff3c6b7cbd6b76
harshiuma <harshitalalchandandani@uma.es>
2026-05-09 18:14:33 +0200

    feat(adapter): añadir adaptador del hospital para la calculadora de salud

---
+++ python-project-healthcalc/healthcalc/health_calc_adapter.py
@@ -0,0 +1,28 @@
+from .health_calc_interface import HealthHospital
+from .health_calc_singleton import HealthCalcImp
+
+
+class HealthHospitalAdapter(HealthHospital):
+
+    def __init__(self):
+        self.calculadora = HealthCalcImp()
+
+    def indiceMasaCorporal(self, altura: float, peso: int) -> tuple[float, str]:
+        peso_kg = peso / 1000
+
+        bmi = self.calculadora.bmi(peso_kg, altura)
+        clasificacion = self.calculadora.bmi_classification(bmi)
+
+        return bmi, clasificacion
+
+    def pesoCorporalIdeal(self, genero: str, altura: float) -> int:
+        altura_cm = altura * 100
+
+        if genero.upper() == "H":
+            sexo = "male"
+        elif genero.upper() == "M":
+            sexo = "female"
+        else:
+            raise ValueError("Error: genero debe ser 'H' o 'M'.")
+
+        return int(self.calculadora.ibw(altura_cm, sexo))
