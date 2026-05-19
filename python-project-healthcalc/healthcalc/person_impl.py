24ceb2409d85333d622b47d712d82ad4bc8e574b
lydiaalbx <lydiaalrojano@gmail.com>
2026-05-19 14:22:00 +0200

    refactor: añadir abstracción Person

---
+++ python-project-healthcalc/healthcalc/person_impl.py
@@ -0,0 +1,30 @@
+from .person import Person
+from .gender import Gender
+
+
+class PersonImpl(Person):
+
+    def __init__(
+        self,
+        weight: float,
+        height: float,
+        gender: Gender,
+        age: int
+    ):
+
+        self._weight = weight
+        self._height = height
+        self._gender = gender
+        self._age = age
+
+    def weight(self) -> float:
+        return self._weight
+
+    def height(self) -> float:
+        return self._height
+
+    def gender(self) -> Gender:
+        return self._gender
+
+    def age(self) -> int:
+        return self._age
