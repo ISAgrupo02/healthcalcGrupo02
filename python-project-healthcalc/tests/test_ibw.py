95b9e853a22069850cc5bb06c4ddf4409b924b24
safiiita <alramasafa@gmail.com>
2026-05-25 12:22:34 +0200

    refactor(test): adaptar pruebas ibw a Person

--- python-project-healthcalc/tests/test_ibw.py
+++ python-project-healthcalc/tests/test_ibw.py
@@ -4,6 +4,8 @@ from healthcalc.health_calc_impl import HealthCalcImpl
 from healthcalc.exceptions import InvalidHealthDataException
 from healthcalc.gender import Gender
 from healthcalc.person_impl import PersonImpl
+from healthcalc.person_impl import PersonImpl
+from healthcalc.gender import Gender
 
 
 class TestIBW:
@@ -16,38 +18,32 @@ class TestIBW:
     # --- IBW Calculation Tests ---
 
     def test_ibw_valido_hombre(self):
-        """Cálculo válido de IBW para hombre."""
-
         height = 175.0
 
         person = PersonImpl(
-            70.0,
-            height,
-            Gender.MALE,
-            30
+           weight=70.0,
+           height=height,
+           gender=Gender.MALE,
+           age=30
         )
 
         expected_ibw = 50 + 0.9 * (height - 152.4)
-
-        result = self.health_calc.ibw_person(person)
+        result = self.health_calc.ideal_body_weight(person)
 
         assert result == expected_ibw
 
     def test_ibw_valido_mujer(self):
-        """Cálculo válido de IBW para mujer."""
-
         height = 160.0
 
         person = PersonImpl(
-            60.0,
-            height,
-            Gender.FEMALE,
-            25
-        )
+           weight=60.0,
+           height=height,
+           gender=Gender.FEMALE,
+           age=25
+       )
 
         expected_ibw = 45.5 + 0.9 * (height - 152.4)
-
-        result = self.health_calc.ibw_person(person)
+        result = self.health_calc.ideal_body_weight(person)
 
         assert result == expected_ibw
 
